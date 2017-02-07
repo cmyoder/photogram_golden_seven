320e4636dd4a7fc9cf5ce52b45609f1a587889dc
cmyoder <cmyoder@uchicago.edu>
2017-02-06 19:12:07 -0600

    I can display photos and caps now

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,11 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo_id = params[:id].to_i
+    @photo_url = Photo.find(@photo_id).source
+    @photo_caption = Photo.find(@photo_id).caption
+  end
+
 
 end
